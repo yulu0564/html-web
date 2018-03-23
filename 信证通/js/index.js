bd37f0b2dce2c3b6bda909f58ffd53d2eeb546c4
yu.lu <yu.lu@jyall.com>
2018-03-23 17:45:03 +0800

    太简单吗

--- 信证通/js/index.js
+++ 信证通/js/index.js
@@ -31,21 +31,39 @@ $(function () {
 
     })
 
-    $('.show').hover(function () {
+    $('.spanRight .show').hover(function () {
         $('.spanRight h5').css('text-align', 'left');
         $('.spanRight').css('background', "rgba(24,44,82,0.5)");
         $('.spanRight h5').css('background', "rgba(24,44,82,0)").animate({'margin-top': '0px'}, 500);
-        $('.showText').css("display", "block");
+        $('.spanRight .showText').css("display", "block");
     })
     $('.spanRight').mouseleave(function () {
         $('.spanRight h5').css('text-align', 'center');
         $('.spanRight').css('background', "rgba(24,44,82,0)");
-        var divh = $("#business").height();
         if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+            var divh = $("#business").height();
             $('.spanRight h5').animate({'margin-top': divh * 0.431 + 'px'}).css('background', "rgba(24,44,82,0.5)");
         } else {
             $('.spanRight h5').animate({'margin-top':'220px'}).css('background',"rgba(24,44,82,0.5)");
         }
-        $('.showText').css("display", "none");
+        $('.spanRight .showText').css("display", "none");
+    })
+
+    $('.spanLeft .show').hover(function () {
+        $('.spanLeft h5').css('text-align', 'left');
+        $('.spanLeft').css('background', "rgba(24,44,82,0.5)");
+        $('.spanLeft h5').css('background', "rgba(24,44,82,0)").animate({'margin-top': '0px'}, 500);
+        $('.spanLeft .showText').css("display", "block");
+    })
+    $('.spanLeft').mouseleave(function () {
+        $('.spanLeft h5').css('text-align', 'center');
+        $('.spanLeft').css('background', "rgba(24,44,82,0)");
+        if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+            var divh = $("#business").height();
+            $('.spanLeft h5').animate({'margin-top': divh * 0.431 + 'px'}).css('background', "rgba(24,44,82,0.5)");
+        } else {
+            $('.spanLeft h5').animate({'margin-top':'220px'}).css('background',"rgba(24,44,82,0.5)");
+        }
+        $('.spanLeft .showText').css("display", "none");
     })
 })
